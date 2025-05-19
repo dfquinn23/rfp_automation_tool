104931e1075f9d4a08c208c14a3f13940bf1df34
dfquinn23 <dfq23@yahoo.com>
2025-05-19 13:19:29 -0500

    Cleaning things up

--- qdrant_embed_check.py
+++ qdrant_embed_check.py
@@ -1,5 +1,8 @@
+import os
 from qdrant_client import QdrantClient
 
-client = QdrantClient(url="https://295f06f4-25f8-4197-bd5e-2227d256a1f1.us-east4-0.gcp.cloud.qdrant.io",
-                      api_key="eREMOVED")
+client = QdrantClient(
+    url="https://295f06f4-25f8-4197-bd5e-2227d256a1f1.us-east4-0.gcp.cloud.qdrant.io")
 print(client.get_collections())
+
+api_key = os.getenv("QDRANT_API_KEY")
